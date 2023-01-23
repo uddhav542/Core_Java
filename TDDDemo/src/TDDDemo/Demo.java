29612aed164386ab33d709bc5031e42873fd54d5
Uddhav <uddhavpatil401@gmail.com>
2023-01-23 11:50:22 +0530

    changes

--- TDDDemo/src/TDDDemo/Demo.java
+++ TDDDemo/src/TDDDemo/Demo.java
@@ -2,6 +2,8 @@ package TDDDemo;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
 
 public class Demo {
@@ -15,6 +17,13 @@ public class Demo {
 	void testAdd()
 	{
 		assertEquals(add("3,4,5"),12);
+	}
+	@Test
+	void testPrimeFactors() {
+		ArrayList<Integer> data = new ArrayList<Integer>();
+		data.add(2);
+		data.add(3);
+		assertEquals(PrimeFactors(6),data);
 	}
 	
 	public String greetings(String name) {
@@ -35,5 +44,30 @@ public class Demo {
 			sum += Integer.parseInt(nums[i]);;
 		}
 		return sum;
+	}
+	
+	public ArrayList<Integer> PrimeFactors(int number)
+	{
+		ArrayList<Integer> ans = new ArrayList<Integer>();
+		for(int i=2;i<number;i++)
+		{
+			if(number%i == 0)
+			{
+				if(isPrime(i))
+					ans.add(i);
+			}
+		}
+		return ans;
+	}
+	boolean isPrime(int num)
+	{
+		for(int i=2;i<=num/2;i++)
+		{
+			if(num%i == 0)
+			{		
+				return false;
+			}
+		}
+		return true;
 	}
 }
